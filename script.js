7a5e1bbfecf0dc95e232b3c02c272646e86da9b7
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-11 15:19:03 -0600

    refactored to get json obj like in the exercises. Want to see if there's a way of accessing the json object outside of the .then() function, though.

--- script.js
+++ script.js
@@ -13,15 +13,34 @@
 */
 
 window.addEventListener("load", (event) => {
-    console.log(event);
-    let form = document.querySelector("form");
+    const fetchPromise = fetch("https://handlers.education.launchcode.org/static/planets.json")
+    const destinationElement = document.getElementById("destination")
+    const planetSelect = document.getElementById("planet-select")
 
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()
-        
-        console.log("form submitted")
-        
+    fetchPromise.then((response) => {
+        const jsonPromise = response.json()
+        console.log(response)
 
-    })
+        jsonPromise.then((json) => {
+            console.log(json);
+
+            let index = Math.floor((Math.random() * 6))
+            
+            //TO DO: make this modular by adding the code to fetchPlanet.js and importing
+            
+            planetSelect.addEventListener("click", (event) => {
+                destinationElement.innerHTML = `<h3> 
+                    Planet: ${json[index].name} <br />
+                    Distance: ${json[index].distance}<br />
+                    Moons: ${json[index].moons} <br />
+                    Star: ${json[index].star}
+                    Diameter: ${json[index].diameter} <br />
+                    </h3><br />
+                    <img height = "250px" src = ${json[index].image}>`;
+                    index = Math.floor((Math.random() * 6))
+                });
+        });
+    });
+    // get the form and validate it down here
 })
