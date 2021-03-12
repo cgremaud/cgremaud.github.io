2bb35af9174dea237c8516b6ff953e90eb790c21
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-11 18:18:31 -0600

    minor reformats

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ window.addEventListener("load", (event) => {
                     </h3><br />
                     <img src = ${json[index].image}>`;
                     index = Math.floor((Math.random() * 6))
-                });
+            });
         });
     });
     // get the form and validate it down here
@@ -46,7 +46,7 @@ window.addEventListener("load", (event) => {
 
         if (!isNaN(pilotName.value)){
             event.preventDefault()
-            alert("Pilot name must be a valid string")
+            // alert("Pilot name must be a valid string")
             faultyItems.style.visibility = "visible"
             let pilotStatus = document.getElementById("pilotStatus")
             pilotStatus.innerHTML = "Pilot not ready."
@@ -57,7 +57,7 @@ window.addEventListener("load", (event) => {
 
         if (!isNaN(copilotName.value)){
             event.preventDefault()
-            alert("Copilot name must be a valid string")
+            // alert("Copilot name must be a valid string")
             faultyItems.style.visibility = "visible"
             let copilotStatus = document.getElementById("copilotStatus")
             copilotStatus.innerHTML = "Copilot not ready."
@@ -65,28 +65,27 @@ window.addEventListener("load", (event) => {
             launchStatus.style.color = "red"
         }
 
-        if (fuelLevel.value < 10000) {
+        if (fuelLevel.value < 10000 || isNaN(fuelLevel.value)) {
             event.preventDefault()
             faultyItems.style.visibility = "visible"
             let fuelStatusElement = document.getElementById("fuelStatus")
-            fuelStatusElement.innerHTML = "Fuel level too low"
+            fuelStatusElement.innerHTML = "Fuel level too low/invalid value"
             launchStatus.style.color = "red"
         }
 
-        if (cargoMass.value > 10000){
+        if (cargoMass.value > 10000 || isNaN(cargoMass.value)){
             faultyItems.style.visibility = "visible"
             let cargoStatusElement = document.getElementById("cargoStatus");
-            cargoStatusElement.innerHTML = "Cargo mass too high";
+            cargoStatusElement.innerHTML = "Cargo mass too high/invalid value";
             event.preventDefault()
             console.log("conditional triggered")
             launchStatus.style.color = "red"
             
         } else {
-            
             event.preventDefault()
             faultyItems.style.visibility ="visible"
         }
 
-
     })
+
 }) 
