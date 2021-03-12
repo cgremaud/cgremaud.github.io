8198d0f0b4cdfc21e39486c7c0302a13c24abd7c
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-12 06:43:25 -0600

    minor wording changes

--- script.js
+++ script.js
@@ -71,14 +71,14 @@ window.addEventListener("load", (event) => {
             event.preventDefault()
             faultyItems.style.visibility = "visible"
             let fuelStatusElement = document.getElementById("fuelStatus")
-            fuelStatusElement.innerHTML = "Fuel level too low/invalid value"
+            fuelStatusElement.innerHTML = "Fuel level too low."
             launchStatus.style.color = "red"
         }
 
         if (cargoMass.value > 10000){
             faultyItems.style.visibility = "visible"
             let cargoStatusElement = document.getElementById("cargoStatus");
-            cargoStatusElement.innerHTML = "Cargo mass too high/invalid value";
+            cargoStatusElement.innerHTML = "Cargo mass too high.";
             event.preventDefault()
             console.log("conditional triggered")
             launchStatus.style.color = "red"
