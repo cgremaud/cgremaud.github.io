e733a9151d829b24e7b00004663f6de49c453f4a
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-15 09:25:43 -0500

    refactored script.js to move some declarations around and reset faulty items text if all conditions are met on a second submit.

--- script.js
+++ script.js
@@ -10,9 +10,14 @@ window.addEventListener("load", (event) => {
     let copilotName = document.querySelector("input[name=copilotName]");
     let fuelLevel = document.querySelector("input[name=fuelLevel]")
     let cargoMass = document.querySelector("input[name=cargoMass]")
+
+    let copilotStatus = document.getElementById("copilotStatus")
+    let pilotStatus = document.getElementById("pilotStatus")
+    let fuelStatusElement = document.getElementById("fuelStatus")
+    let cargoStatusElement = document.getElementById("cargoStatus");
     
-    let faultyItems = document.getElementById("faultyItems")
-    let launchStatus = document.getElementById("launchStatus")
+    const faultyItems = document.getElementById("faultyItems")
+    const launchStatus = document.getElementById("launchStatus")
 
     fetchPromise.then((response) => {
         const jsonPromise = response.json()
@@ -55,7 +60,7 @@ window.addEventListener("load", (event) => {
             event.preventDefault()
             // alert("Pilot name must be a valid string")
             faultyItems.style.visibility = "visible"
-            let pilotStatus = document.getElementById("pilotStatus")
+            
             pilotStatus.innerHTML = "Pilot not ready."
             console.log("Conditional Triggered")
             launchStatus.innerHTML= "LAUNCH ABORTED"
@@ -66,31 +71,41 @@ window.addEventListener("load", (event) => {
             event.preventDefault()
             // alert("Copilot name must be a valid string")
             faultyItems.style.visibility = "visible"
-            let copilotStatus = document.getElementById("copilotStatus")
+            
             copilotStatus.innerHTML = "Copilot not ready."
             console.log("Conditional Triggered")
+            launchStatus.innerHTML= "LAUNCH ABORTED"
             launchStatus.style.color = "red"
         }
 
         if (fuelLevel.value < 10000) {
             event.preventDefault()
             faultyItems.style.visibility = "visible"
-            let fuelStatusElement = document.getElementById("fuelStatus")
+            
             fuelStatusElement.innerHTML = "Fuel level too low."
+            launchStatus.innerHTML= "LAUNCH ABORTED"
             launchStatus.style.color = "red"
         }
 
         if (cargoMass.value > 10000){
             faultyItems.style.visibility = "visible"
-            let cargoStatusElement = document.getElementById("cargoStatus");
+            
             cargoStatusElement.innerHTML = "Cargo mass too high.";
             event.preventDefault()
             console.log("conditional triggered")
+            launchStatus.innerHTML= "LAUNCH ABORTED"
             launchStatus.style.color = "red"
             
         } else {
             event.preventDefault()
+            pilotStatus.innerHTML = "Pilot ready."
+            copilotStatus.innerHTML = "Copilot not ready."
+            fuelStatusElement.innerHTML = "Fuel level nominal."
+            cargoStatusElement.innerHTML = "Cargo mass nominal.";
+
+            
             faultyItems.style.visibility ="visible"
+
           
         }
     });
